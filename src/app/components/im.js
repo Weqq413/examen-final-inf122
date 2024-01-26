6e322832b391ea203825a5e80a63f03561b60589
Weqq413 <155451159+Weqq413@users.noreply.github.com>
2024-01-26 15:52:16 +0000

    commm

--- src/app/components/im.js
+++ src/app/components/im.js
@@ -11,6 +11,10 @@ function Im (){
     const [alto, setAlto] = useState("alto");
     const [ancho, setAncho] = useState("ancho");
     const [id, setId ] = useState("id");
+    const [ataque, setAtaque ] = useState("ataque");
+    const [defenza, setDefenza ] = useState("defenza");
+    const [sp, setSp  ] = useState("sp");
+    
 
  
     const url="https://pokeapi.co/api/v2/pokemon/65";    
@@ -22,23 +26,29 @@ function Im (){
             setEstado(data.species.name),
             setHabilidad(data.moves[0].move.name),
             setAlto(data.game_indices[0].game_index),
-            setAncho(data.types.weight),
-            setHp(data.moves[0].move.order),
-            setId(data.held_items.id)
+            setAncho(data.types[0].type.weight),
+            setHp(data.stats[0].base-stats),
+            setId(data.game_indices[3].game_index),
+            setAtaque(data.stats[0].base-stats),
+            setDefenza(data.stats[0].base-stats),
+            setSp(data.stats[0].base-stats)
         });
 },[]);
 return(
     <div className={style.contenedor}>
         <h1>My pokemon</h1>
         <h1>{estado}</h1>
-        <h1> {id}</h1>
+        <p> {id}</p>
         <Image src={pokemon} height={300} width={300}/>
         <h3>about</h3>
-        <h3>height:  {alto}</h3>
-        <h3>weight:  {ancho}</h3>
+        <h3>height:     {alto}</h3>
+        <h3>weight:     {ancho}</h3>
         <h3>habilidad:  {habilidad}</h3>
-
-        <h3>hp: {hp}</h3>
+        <h3>Stats</h3>
+        <h3>hp:         {hp}</h3>
+        <h3>ataque:     {ataque}</h3>
+        <h3>defense:     {defenza}</h3>
+        <h3>Speed:     {sp}</h3>
 
     
     </div>
