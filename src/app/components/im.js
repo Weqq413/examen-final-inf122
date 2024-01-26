443b64856735d403d0e85f72484c5c515ec9f2f3
Weqq413 <155451159+Weqq413@users.noreply.github.com>
2024-01-26 15:09:11 +0000

    commm

--- src/app/components/im.js
+++ src/app/components/im.js
@@ -7,6 +7,12 @@ function Im (){
     const [pokemon,setPokemon]=useState("/vercel.svg");
     const [estado, setEstado]=useState("esperando");
     const [habilidad, setHabilidad] = useState("habilidad");
+    const [hp, setHp ] = useState("hp");
+    const [alto, setAlto] = useState("alto");
+    const [ancho, setAncho] = useState("ancho");
+    const [id, setId ] = useState("id");
+
+ 
     const url="https://pokeapi.co/api/v2/pokemon/65";    
     useEffect(()=>{
         fetch(url)
@@ -14,16 +20,28 @@ function Im (){
         .then(data=>{
             setPokemon(data.sprites.front_default),
             setEstado(data.species.name),
-            setHabilidad(data.moves[0].move.name)
+            setHabilidad(data.moves[0].move.name),
+            setAlto(data.game_indices[0].game_index),
+            setAncho(data.types[0].weight),
+            setHp(data.moves[0].move.order),
+            setId(data.held_items.id)
         });
 },[]);
 return(
     <div className={style.contenedor}>
+        <h1>My pokemon</h1>
         <h1>{estado}</h1>
-        <Image src={pokemon} height={250} width={250}/>
-        <h1>{habilidad}</h1>
+        <h1> {id}</h1>
+        <Image src={pokemon} height={300} width={300}/>
+        <h3>about</h3>
+        <h3>height:  {alto}</h3>
+        <h3>weight:  {ancho}</h3>
+        <h3>habilidad:  {habilidad}</h3>
+
+        <h3>hp: {hp}</h3>
+
     
-        </div>
+    </div>
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Im from "./components/im";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Im/>
+    <div className={styles.main}>
 
-    </main>
+      <Im/>
+    </div>
   );
 }
